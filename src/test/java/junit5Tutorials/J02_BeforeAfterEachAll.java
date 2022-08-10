c68758d1c67fc2775ee066f3ed157734c14fe0db
Hakan Inal <89166736+Inal2021@users.noreply.github.com>
2022-08-10 11:18:13 +0200

    v10.08.22.1

--- src/test/java/junit5Tutorials/J02_BeforeAfterEachAll.java
+++ src/test/java/junit5Tutorials/J02_BeforeAfterEachAll.java
@@ -1,8 +1,7 @@
 package junit5Tutorials;
 
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,24 +9,61 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class J02_BeforeAfterEachAll {
 
     /*
-    @BeforeEach ==> Her adimdan önce yapilir. Debriyaja basma metaforu.
-    @AfterEach ==> Her adimdan sonra yapilir. Debriyajdan ayagi cekme metaforu.
+    A) @BeforeEach ==> Her adimdan önce yapilir. Debriyaja basma metaforu.
 
-    @BeforeAll ==> Namazda Tekbir getirmek
-    @AfterAll ==> Mamaz sonrasi Selam vermek
+    B) @AfterEach ==> Her adimdan sonra yapilir. Debriyajdan ayagi cekme metaforu.
+
+    C) @BeforeAll ==> Namazda Tekbir getirmek gibidir. Tüm test ögelerinden önce veritabani calistirilmak istenebilir.
+    Veya cloud ile baglanti kurulabilir ki oradan belli degerler alinabilsin. Bu durumda bu annatation kullanilir.
+
+    D) @AfterAll ==> Mamaz sonrasi Selam vermek gibidir. Tüm test ögeleri tamamlandiktan sonra veri tabanini veya cloud baglantisini
+    kesmek icin kullanilir.
+
+    @Before ve @After arasinda creat/yazim siralamasi farketmez. Java After gördügünde bunu en son compile etmesi gerektigini bilir.
+    @BeforeAll ve @AfterAll metodlarinin kullanildigi metodlar static olmalidir.
 
      */
+
+@BeforeAll
+static void awsCloudOpening() {
+    System.out.println("AWS started");
+}
+
+@AfterAll
+static void  awsCloudClosing() {
+    System.out.println("AWS finished");
+}
+
+
+    @BeforeEach
+            void setup(TestInfo info) {  // TestInfo --> @DisplayName degerini return eder.
+        str="Junit5 ile test degerleri";
+        System.out.println("@BeforeEach calisti ve "+info.getDisplayName()+" basarili oldu.");
+    }
+
+    @AfterEach
+            void clean() {
+        str="Default olarak bu metne döndüm.";
+        System.out.println(str);
+
+    }
+
+
+    String str="";
+
 
     @Test
     @DisplayName("Length metod Test")
     void testStringLength(){
 
-        String str="Herkes bir gün Java ögrenecek";
+
 
         int actual = str.length();
-        int expected = 29;
+        int expected = 25;
 
         assertEquals(expected, actual,"Degerler esit degil");
+
+        System.out.println("1. metod calisti");
     }
 
 
@@ -35,12 +71,13 @@ public class J02_BeforeAfterEachAll {
     @DisplayName("Arrays Test")
     void testWithArrays(){
 
-        String str="Junit5 ile test degerleri";
+
         String[] actual=str.split(" ");
         String[] expected={"Junit5","ile","test","degerleri"};
 
         assertArrayEquals(expected, actual,"array degerleri esit degil!");
 
+        System.out.println("2. metod calisti");
     }
 
 
