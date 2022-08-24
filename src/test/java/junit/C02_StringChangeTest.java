498b5085752da00de3efa4f102160501a65963c2
Hakan Inal <89166736+Inal2021@users.noreply.github.com>
2022-08-24 12:24:09 +0200

    v24.08.22.5

--- src/test/java/junit/C02_StringChangeTest.java
+++ src/test/java/junit/C02_StringChangeTest.java
@@ -3,20 +3,32 @@ package junit;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class C02_StringChangeTest {
 
+    C02_StringChange strChange=new C02_StringChange();  // Static olmayan class tan method call etmek icin obje olusturudk.
+
     @BeforeEach
     void setUp() {
+        strChange=new C02_StringChange();
+        System.out.println("test verisi CREAT edildi");
     }
 
     @AfterEach
     void tearDown() {
+        strChange=null;
+        System.out.println("test verisi DELETE edildi.");
+        System.out.println("-------*******-------");
     }
 
-    @Test
-    void ilkIkiASil() {
+    @ParameterizedTest
+    @CsvSource(value = {"BC,AABC","B,AB","BCDE,BCDE","'',AA","B,B"}) //hiclik
+    void ilkIkiASil(String kirpilmis, String girdi) {
+        assertEquals(kirpilmis,strChange.ilkIkiASil(girdi));
+        System.out.println("ilk ikide olan A lar silindi");
     }
 }
